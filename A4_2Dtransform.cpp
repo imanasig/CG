40c53e7b8116ba5e69fb8ca41a3403242254fe55
Manasi Gaikwad <imanasig@gmail.com>
2024-12-18 16:30:23 +0530

    Update A4_2Dtransform.cpp

--- A4_2Dtransform.cpp
+++ A4_2Dtransform.cpp
@@ -1,7 +1,6 @@
 #include <GL/glut.h>
 #include <iostream>
 #include <cmath>
-#include <vector>
 using namespace std;
 
 #define PI 3.14159265359
@@ -32,78 +31,67 @@ public:
 // Define a Shape class to hold vertices and apply transformations
 class Shape {
 public:
-    vector<Point> vertices;
+    Point vertices[4];
     Point center;
 
-    // Constructor to initialize a regular polygon with a specified number of sides
-    Shape(int sides) {
-        if (sides < 3) sides = 3; // Minimum of 3 sides for a polygon
-        vertices.resize(sides);
-        float angleIncrement = 2 * PI / sides;
-        
-        // Calculate the vertices for a regular polygon
-        for (int i = 0; i < sides; i++) {
-            float angle = i * angleIncrement;
-            vertices[i] = Point(0.5 * cos(angle), 0.5 * sin(angle));
-        }
+    Shape() {
+        // Define a square as the initial shape
+        vertices[0] = Point(-0.5, -0.5);
+        vertices[1] = Point(0.5, -0.5);
+        vertices[2] = Point(0.5, 0.5);
+        vertices[3] = Point(-0.5, 0.5);
         calculateCenter();
     }
 
     // Calculate the center of the shape
     void calculateCenter() {
-        center.x = 0;
-        center.y = 0;
-        for (const auto& vertex : vertices) {
-            center.x += vertex.x;
-            center.y += vertex.y;
-        }
-        center.x /= vertices.size();
-        center.y /= vertices.size();
+        center.x = (vertices[0].x + vertices[1].x + vertices[2].x + vertices[3].x) / 4.0;
+        center.y = (vertices[0].y + vertices[1].y + vertices[2].y + vertices[3].y) / 4.0;
     }
 
     // Apply translation using operator overloading
     void translate(const Point& translation) {
-        for (auto& vertex : vertices) {
-            vertex = vertex + translation;
+        for (int i = 0; i < 4; i++) {
+            vertices[i] = vertices[i] + translation;
         }
         calculateCenter(); // Update center after translation
     }
 
     // Apply scaling using operator overloading
     void scale(float scale) {
-        for (auto& vertex : vertices) {
-            vertex = (vertex - center) * scale + center;
+        for (int i = 0; i < 4; i++) {
+            vertices[i] = (vertices[i] - center) * scale + center;
         }
     }
 
     // Apply rotation around the center of the shape
-    void rotate(float angle) {
-        float rad = angle * PI / 180.0;  // Convert angle to radians
-        float cosA = cos(rad);
-        float sinA = sin(rad);
+    // void rotate(float angle) {
+    //     float rad = angle * PI / 180.0;  // Convert angle to radians
+    //     float cosA = cos(rad);
+    //     float sinA = sin(rad);
 
-        for (auto& vertex : vertices) {
-            // Translate point to origin (relative to center), rotate, then translate back
-            float x = vertex.x - center.x;
-            float y = vertex.y - center.y;
+    //     for (int i = 0; i < 4; i++) {
+    //         // Translate point to origin (relative to center), rotate, then translate back
+    //         float x = vertices[i].x - center.x;
+    //         float y = vertices[i].y - center.y;
 
-            vertex.x = x * cosA - y * sinA + center.x;
-            vertex.y = x * sinA + y * cosA + center.y;
-        }
-    }
+    //         vertices[i].x = x * cosA - y * sinA + center.x;
+    //         vertices[i].y = x * sinA + y * cosA + center.y;
+    //     }
+    // }
 
     // Function to draw the shape
     void draw() const {
         glBegin(GL_POLYGON);
-        for (const auto& vertex : vertices) {
-            glVertex2f(vertex.x, vertex.y);
+        for (int i = 0; i < 4; i++) {
+            glVertex2f(vertices[i].x, vertices[i].y);
         }
         glEnd();
     }
 };
 
 // Initialize the shape
-Shape* polygon;
+Shape square;
 
 // OpenGL Initialization
 void init() {
@@ -116,7 +104,7 @@ void init() {
 // Display function to render the shape
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-    polygon->draw();
+    square.draw();
     glFlush();
 }
 
@@ -124,39 +112,33 @@ void display() {
 void keyboard(unsigned char key, int x, int y) {
     switch (key) {
         case 't':  // Translate right
-            polygon->translate(Point(0.1, 0.0));
+            square.translate(Point(0.1, 0.0));
             break;
         case 'T':  // Translate left
-            polygon->translate(Point(-0.1, 0.0));
+            square.translate(Point(-0.1, 0.0));
             break;
         case 's':  // Scale up
-            polygon->scale(1.1);
+            square.scale(1.1);
             break;
         case 'S':  // Scale down
-            polygon->scale(0.9);
+            square.scale(0.9);
             break;
-        case 'r':  // Rotate clockwise
-            polygon->rotate(-10);
-            break;
-        case 'R':  // Rotate counter-clockwise
-            polygon->rotate(10);
-            break;
+        // case 'r':  // Rotate clockwise
+        //     square.rotate(-10);
+        //     break;
+        // case 'R':  // Rotate counter-clockwise
+        //     square.rotate(10);
+        //     break;
     }
     glutPostRedisplay();  // Redraw the screen
 }
 
 // Main function to set up OpenGL and run the program
 int main(int argc, char** argv) {
-    int sides;
-    cout << "Enter the number of sides for the polygon (minimum 3): ";
-    cin >> sides;
-
-    cout << "Use the keys to transform the shape:" <<endl;
+    cout << "Use the keys to transform the shape:" << endl;
     cout << "t for translating right, T for left." << endl;
     cout << "s to scale up, S to scale down." << endl;
-    cout << "r to rotate clockwise, R to rotate counter-clockwise." << endl;
-
-    polygon = new Shape(sides);
+   // cout << "r to rotate clockwise, R to rotate counter-clockwise." << endl;
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
@@ -167,7 +149,5 @@ int main(int argc, char** argv) {
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutMainLoop();
-
-    delete polygon;
     return 0;
 }
